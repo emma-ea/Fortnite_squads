d0cb134f1fda8e357516c5c7c1c655b0844cc311
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2026-01-30 10:03:11 +0000

    fix android build issue

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,7 +1,8 @@
 class ApiConfig {
   // Update this to your local IP if testing on a real device
   // e.g. 'http://192.168.1.5:8080/api/v1'
-  static const String baseUrl = 'http://localhost:8080/api/v1'; 
+  // static const String baseUrl = 'http://localhost:8080/api/v1';
+  static const String baseUrl = 'https://mock-0447e83de80f46c59c75c22b636d6dac.mock.insomnia.run/api/v1';
 }
 class ApiConstants {
    static const String baseUrl = ApiConfig.baseUrl;
